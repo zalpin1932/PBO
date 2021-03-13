4288d2c000573359ee0e888e66175a71e94ff848
zalpin1932 <faizalnurarifin5@gmail.com>
2021-03-13 18:43:40 +0700

    project saya

---
+++ kalkulator.java
@@ -0,0 +1,17 @@
+public class kalkulator {
+	public static void main (String args []){
+		int a = 8, b=4;
+		
+		int jumlah = a+b;
+		int kurang = a-b;
+		int kali = a*b;
+		int bagi =a/b;
+		int modulus = a%b;
+		System.out.println("penjulahan :" +jumlah);
+		System.out.println("pengurangan :"+kurang);
+		System.out.println("perkalian "+kali);
+		System.out.println("pembagian :"+bagi);
+		System.out.println("modulus : "+modulus);
+	}
+
+}
